39bb1b56bb27195008c38ea51ebbdd3a9a8b1743
ahmed <ahmed.abdalllaa@gmail.com>
2015-05-23 16:03:01 +0200

    Remplacemment du formulaire CreateUserForm par le formulaire RegisterUserForm

--- projetRecettesDeCuisine/recettesdecuisine/forms.py
+++ projetRecettesDeCuisine/recettesdecuisine/forms.py
@@ -1,18 +1,29 @@
 from django import forms
-from django.forms import ModelForm
 from .models import Recette
 
+from  django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-class CreateUserForm(forms.Form):
+
+class RegisterUserForm(UserCreationForm):
+    # email = forms.EmailField(label=u'Courriel', required=True)
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2", "email")
+
+'''
+#Ancien formulaire de création d'un utilisateur
+class RegisterUserForm(forms.Form):
     username = forms.CharField(label=u'Login', max_length=100)
     password = forms.CharField(label=u'Mot de passe', max_length=100, widget=forms.PasswordInput)
     email = forms.EmailField(label=u'Courriel')
+'''
 
 
 class RecetteForm(forms.ModelForm):
     class Meta:
         model = Recette
-        #fields = ('title', 'type', 'difficulty', 'cost', 'preparationTime', 'cookTime', 'restTime')
+        # fields = ('title', 'type', 'difficulty', 'cost', 'preparationTime', 'cookTime', 'restTime')
         labels = {
             'title': "Titre",
             'difficulty': "Difficulté",
