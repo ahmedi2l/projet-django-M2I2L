47ac317e9b582b7e9fe7933fbff791040cc1febb
ahmed <ahmed.abdalllaa@gmail.com>
2015-05-23 16:05:36 +0200

    Amélioration de la création de compte utilisateur et de l'authentification

--- projetRecettesDeCuisine/recettesdecuisine/urls.py
+++ projetRecettesDeCuisine/recettesdecuisine/urls.py
@@ -6,12 +6,13 @@ from django.contrib.auth import views as aviews
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
-                       url(r'^ajoutRecettes$', views.ajoutRecettes, name='ajoutRecettes'),
+                       url(r'^ajoutRecettes$', views.ajoutRecette, name='ajoutRecettes'),
 
                        # url(r'^login/$', views.login, name='login'),
                        # url(r'^auth/$', views.auth_view, name='auth_view'),
                        url(r'^logout/$', views.logout, name='logout'),
                        url(r'^loggedin/$', views.loggedin, name='loggedin'),
                        url(r'^index/$', views.index, name='index'),
-                       url(r'^createuser/$', views.createuser, name='createuser'),
+                       url(r'^registerUser/$', views.registerUser, name='registerUser'),
+                       url(r'^registerUser_success/$', views.registerUser_success, name='registerUser_success'),
                        )
--- projetRecettesDeCuisine/recettesdecuisine/views.py
+++ projetRecettesDeCuisine/recettesdecuisine/views.py
@@ -5,17 +5,15 @@ from .forms import RecetteForm
 
 from django.contrib import auth
 from django.core.context_processors import csrf
-from recettesdecuisine.forms import CreateUserForm
-from django.contrib.auth.models import User
+from recettesdecuisine.forms import RegisterUserForm
 
 # Create your views here.
 
-# @login_required()
 def index(request):
     return render(request, 'recettesdecuisine/index.html', )
 
-
-def ajoutRecettes(request):
+# Ajout d'une recette
+def ajoutRecette(request):
     if request.method == 'POST':  # S'il s'agit d'une requête POST
         formulaire = RecetteForm(request.POST)  # Nous reprenons les données
         if formulaire.is_valid():  # Nous vérifions que les données envoyées sont valides
@@ -28,41 +26,6 @@ def ajoutRecettes(request):
     }
     return render(request, 'recettesdecuisine/ajoutRecettes.html', context)
 
-
-###
-"""
-def login(request):
-    if request.method == "POST":
-        form = LoginForm(request.POST)
-
-        if form.is_valid():
-
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-
-            user = auth.authenticate(username=username, password=password)
-
-            context = {
-                'username': username,
-                'password': password,
-                'result': user,
-            }
-
-            if user is None:
-                return render(request, 'registration/login_ok.html', context)
-            else:
-                return render(request, 'registration/loggedin.html', context)
-
-    else:
-        form = LoginForm()
-
-    context = {
-        'form': form,
-    }
-    return render(request, 'registration/login_ok.html', context)
-"""
-
-###
 @login_required()
 def loggedin(request):
     context = {
@@ -70,30 +33,49 @@ def loggedin(request):
     }
     return render_to_response('registration/loggedin.html', context)
 
-
 def logout(request):
     return render(request, 'registration/logged_out.html')
 
 
-def createuser(request):
+# Création d'un compte utilisateur
+def registerUser(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("/registerUser_success")
+    else:
+        form = RegisterUserForm()
+    return render(request, "registration/registerUser.html", {
+        'form': form,
+    })
+
+
+def registerUser_success(request):
+    return render(request, 'registration/registerUser_success.html', )
+
+
+'''
+#Ancienne méthonde de création d'un utilisateur
+def registerUser(request):
     if request.method == "POST":
-        form = CreateUserForm(request.POST)
+        form = RegisterUserForm(request.POST)
 
         if form.is_valid():
-
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            email = request.POST.get('email')
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
 
             try:
                 user = User.objects.create_user(username=username, email=email, password=password)
-                return render(request, 'registration/createuser.html', {'username': username, 'message': u"Le compte a été crée"})
+                return render(request, 'registration/registerUser_success.html', {'username': username, 'message': u"Le compte a été crée"})
             except :
-                return render(request, 'registration/createuser.html', {'username': username, 'message': u"Le compte n'a pas pu être créé"})
+                return render(request, 'registration/registerUser_success.html', {'username': username, 'message': u"Le compte n'a pas pu être créé"})
     else:
-        form = CreateUserForm()
+        form = RegisterUserForm()
 
     context = {
         'form': form,
     }
-    return render(request, 'registration/createuser.html', context)
+    return render(request, 'registration/registerUser.html', context)
+'''
