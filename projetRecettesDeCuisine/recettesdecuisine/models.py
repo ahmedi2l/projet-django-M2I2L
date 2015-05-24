2d27627c092ea97a1bdd0d11dc9cbc1c67e30c25
ahmed <ahmed.abdalllaa@gmail.com>
2015-05-25 01:20:53 +0200

    Ajout des champs owner et owner_id

--- projetRecettesDeCuisine/recettesdecuisine/models.py
+++ projetRecettesDeCuisine/recettesdecuisine/models.py
@@ -1,32 +1,35 @@
 from django.db import models
 from django.core import validators
 
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
-
-
 class Recette(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    TYPE_CHOICES=(
-        ('entre','Entrée'),
-        ('platPrincipal','Plat principal'),
-        ('dessert','Dessert'),
+    TYPE_CHOICES = (
+        ('entre', 'Entrée'),
+        ('platPrincipal', 'Plat principal'),
+        ('dessert', 'Dessert'),
     )
     type = models.CharField(max_length=30, choices=TYPE_CHOICES, default='platPrincipal')
-    DIFFICULTY_CHOICES=(
-        ('simple','Simple'),
-        ('moyen','Moyen'),
-        ('difficile','Difficile'),
+    DIFFICULTY_CHOICES = (
+        ('simple', 'Simple'),
+        ('moyen', 'Moyen'),
+        ('difficile', 'Difficile'),
     )
-    difficultyLevel= models.CharField(max_length=30, choices=DIFFICULTY_CHOICES, default='simple')
+    difficultyLevel = models.CharField(max_length=30, choices=DIFFICULTY_CHOICES, default='simple')
     cost = models.DecimalField(max_digits=5, decimal_places=2, validators=[validators.MinValueValidator(0)])
-    #images = FileField(upload_to='images/recettes')
+    # images = FileField(upload_to='images/recettes')
     preparationTime = models.PositiveIntegerField(blank=True, null=True)
     cookTime = models.PositiveIntegerField(blank=True, null=True)
     restTime = models.PositiveIntegerField(blank=True, null=True)
     creationDate = models.DateField(auto_now_add=True)
     midificationDate = models.DateField(auto_now=True, default=creationDate)
+    owner_id = models.PositiveIntegerField(blank=True)
+    #owner = models.ForeignKey(User, blank=True,null=True)
+    owner = models.CharField(max_length=200, blank=True)
 
     '''
     ingredientsListe = models.CharField(max_length=200)
@@ -35,5 +38,5 @@ class Recette(models.Model):
     comment = models.CharField(max_length=200)
     '''
 
-    def __str__(self):              # __unicode__ on Python 2
+    def __str__(self):  # __unicode__ on Python 2
         return self.title
