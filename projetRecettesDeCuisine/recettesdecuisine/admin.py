882bd4767f2c17ffd098f2276452986ca3469527
ahmed <ahmed.abdalllaa@gmail.com>
2015-05-25 16:30:35 +0200

    Déplacement de la classe RecetteAdmin et ajout de la methode save_model

--- projetRecettesDeCuisine/recettesdecuisine/admin.py
+++ projetRecettesDeCuisine/recettesdecuisine/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from recettesdecuisine.models import Recette
+
 # Register your models here.
+
+class RecetteAdmin(admin.ModelAdmin):
+    fields = ('title', 'type', 'difficultyLevel', 'cost', 'preparationTime',
+              'cookTime', 'restTime')  # , 'owner')
+
+    # Remplissage automatique du champ owner lors de l'ajout d'une rectte via l'admin
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        obj.save()
+
+
+admin.site.register(Recette, RecetteAdmin)
