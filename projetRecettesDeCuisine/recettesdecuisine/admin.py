358a440a7c53880c9c12edde069cb1c46cdf65ab
ahmed <ahmed.abdalllaa@gmail.com>
2015-05-25 17:11:58 +0200

    Ajout du remplissage automatique du champ ownerId de la recette

--- projetRecettesDeCuisine/recettesdecuisine/admin.py
+++ projetRecettesDeCuisine/recettesdecuisine/admin.py
@@ -11,6 +11,7 @@ class RecetteAdmin(admin.ModelAdmin):
     # Remplissage automatique du champ owner lors de l'ajout d'une rectte via l'admin
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
+        obj.ownerId = request.user.id
         obj.save()
 
 
