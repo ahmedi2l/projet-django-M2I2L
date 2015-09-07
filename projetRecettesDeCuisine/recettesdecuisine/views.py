b87ffc557299c00dc2a2b46f8f1c648fd025687b
ahmed <ahmed.abdallah@cliss21.org>
2015-09-07 11:11:45 +0200

    Encodage en utf-8

--- projetRecettesDeCuisine/recettesdecuisine/views.py
+++ projetRecettesDeCuisine/recettesdecuisine/views.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 from django.shortcuts import render, render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
